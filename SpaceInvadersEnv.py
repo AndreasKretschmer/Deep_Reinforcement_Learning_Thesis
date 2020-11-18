8217e6b6d6e7753be878f9f9574d11f8187a2dca
Andreas Kretschmer <andy.kretschmer@web.de>
2020-11-18 20:55:46 +0100

    RL: Test the environment

--- SpaceInvadersEnv.py
+++ SpaceInvadersEnv.py
@@ -4,8 +4,10 @@ env = gym.make('SpaceInvaders-v0')
 # env.reset()
 
 print(env.action_space) #actionspace of 6 actions
+print(env.observation_space) #Box(210, 160, 3) = the givenpicture 210 x 160 pixels 3 colours
 
 for episode in range(2):
+    break
     observation = env.reset()
     for _ in range(1000):
         #render game
@@ -22,6 +24,7 @@ for episode in range(2):
         print('done: {}'.format(done))
         print('info: {}'.format(info))
 
+        #the game is lost, if the player has 0 lives left
         if info['ale.lives'] == 0:
             break
 
