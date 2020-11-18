a6a1ada5978e7b4e162884cad1e9ad0906abfcb1
Andreas Kretschmer <andy.kretschmer@web.de>
2020-11-18 20:46:22 +0100

    RL: testing the environment

--- SpaceInvadersEnv.py
+++ SpaceInvadersEnv.py
@@ -1,12 +1,28 @@
 import gym
 
 env = gym.make('SpaceInvaders-v0')
-env.reset()
+# env.reset()
 
-print(env.action_space)
+print(env.action_space) #actionspace of 6 actions
 
-for _ in range(2000):
-    env.render()
-    env.step(env.action_space.sample())
+for episode in range(2):
+    observation = env.reset()
+    for _ in range(1000):
+        #render game
+        env.render()
+
+        #print observation
+        print(observation)
+
+        #do step and get new observation, reward, state and terminationstate
+        observation, reward, done, info   = env.step(env.action_space.sample())
+        print(reward)
+        #print values after each step
+        print('reward: {:f}'.format(reward))
+        print('done: {}'.format(done))
+        print('info: {}'.format(info))
+
+        if info['ale.lives'] == 0:
+            break
 
 env.close()
